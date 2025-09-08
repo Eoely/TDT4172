78e0cf4c8377d22eb41d6ff58a8609a5d891b407
Eoely <eivind.ostlyngen@hotmail.no>
2025-09-08 11:38:50 +0200

    Passing accuracy, kms

--- a1/logistical_regression.py
+++ a1/logistical_regression.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 class LogisticRegression:
-    def __init__(self, learning_rate: float = 0.1, epochs: int = 1000):
+    def __init__(self, learning_rate: float = 0.1, epochs: int = 1500):
         # Sett før. Hyperparameter som styrer skritt-lengden
         self.learning_rate = learning_rate
         # Antall ganger treningsdataene sendes gjennom læringsalgoritmen. Nok en hyperparameter.
@@ -12,13 +12,23 @@ class LogisticRegression:
         self.weights, self.bias = None, None
         self.losses, self.train_accuracies = [], []
 
+    def accuracy(self, y, y_pred):
+        return np.mean(y == y_pred)
+
     def _sigmoid(self, x: np.ndarray) -> np.ndarray:
         return 1/(1 + np.e**-x)
 
+    def _compute_loss(self, y, y_pred, eps=1e-12):
+        p = np.clip(y_pred, eps, 1.0 - eps)
+        return -np.mean(y*np.log(p) + (1-y) * np.log(1 - p))
+
     def compute_gradients(self, x: np.ndarray, y: np.ndarray, y_pred: np.ndarray):
-        loss = y - y_pred
-        grad_w = np.mean(np.matmul(loss, x))
-        grad_b = np.mean(loss)
+        loss = y_pred - y
+        # grad_w = np.mean(np.matmul(loss, x))
+        # grad_b = np.mean(loss)
+
+        grad_w = 2 * np.matmul(x.transpose(), loss) / x.shape[0]
+        grad_b = 2 * np.mean(loss)
         return grad_w, grad_b
 
     def update_parameters(self, grad_w: float, grad_b: float) -> None:
@@ -26,26 +36,31 @@ class LogisticRegression:
         self.bias = self.bias - self.learning_rate * grad_b
 
     def accuracy(self, true_values: np.ndarray, predictions: np.ndarray) -> float:
-        return float(np.mean(true_values == predictions))
+        return np.mean(true_values == predictions)
 
     def fit(self, df_x: pd.Series, df_y: pd.Series):
         # x.shape = [#datapunkter, #features] -> weights er en vektor med size = antall features
         self.bias = 0
         x = np.asarray(df_x)
-        y = np.asarray(df_y).reshape(-1, 1)
+        y = np.asarray(df_y).reshape(-1,)
         self.weights = np.zeros(x.shape[1])
         self.weights = self.weights
         # Gradient Descent
         for _ in range(self.epochs):
-            lin_model = np.dot(x, self.weights) + self.bias
+            lin_model = np.matmul(x, self.weights) + self.bias
             y_pred = self._sigmoid(lin_model)
             grad_w, grad_b = self.compute_gradients(x, y, y_pred)
+
             self.update_parameters(grad_w, grad_b)
+            loss = self._compute_loss(y, y_pred)
+            pred_to_class = [1 if _y > 0.5 else 0 for _y in y_pred]
+            self.train_accuracies.append(self.accuracy(y, pred_to_class))
+            self.losses.append(loss)
 
     def predict(self, df_x):
         x = np.asarray(df_x)
 
-        lin_model = np.dot(x, self.weights) + self.bias
+        lin_model = np.matmul(x, self.weights) + self.bias
         # lin_model = np.matmul(x, self.weights.transpose()) + self.bias
         y_pred = self._sigmoid(lin_model)
         return [1 if _y > 0.5 else 0 for _y in y_pred]
